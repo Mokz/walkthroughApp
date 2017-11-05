9c87fd5760c556d283c2970ec5dd7c505eb6b7e6
Mokz <miguel95.mazy@gmail.com>
2017-11-04 23:58:50 -0500

    Eliminar comentarios

--- app/src/main/java/com/mokz/walkthroughapp/MainActivity.java
+++ app/src/main/java/com/mokz/walkthroughapp/MainActivity.java
@@ -1,18 +1,14 @@
 package com.mokz.walkthroughapp;
 
-//import android.content.SharedPreferences;
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-//import android.support.v7.widget.Toolbar;
-//import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    //    boolean firstStart;
     Button btn_Repetir;
 
     @Override
@@ -25,11 +21,10 @@ public class MainActivity extends AppCompatActivity {
         btn_Repetir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // We normally won't show the welcome slider again in real app
-                // but this is for testing
+                // Para ejecutar varias veces el walkthrough
                 PrefManager prefManager = new PrefManager(getApplicationContext());
 
-                // make first time launch TRUE
+                // le da TRUE la primera vez que lo ejecuta
                 prefManager.setFirstTimeLaunch(true);
 
                 startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
@@ -37,31 +32,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        findViewById(R.id.btn_play_again).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // We normally won't show the welcome slider again in real app
-//                // but this is for testing
-//                PrefManager prefManager = new PrefManager(getApplicationContext());
-//
-//                // make first time launch TRUE
-//                prefManager.setFirstTimeLaunch(true);
-//
-//                startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
-//                finish();
-//            }
-//        });
-
-       /* SharedPreferences settings = getSharedPreferences("PREFS",0);
-        firstStart = settings.getBoolean("first_time_start",true);
-
-        if (firstStart) {
-            SharedPreferences.Editor editor = settings.edit();
-            editor.putBoolean("first_time_start",false);
-            editor.commit();
-
-            Toast.makeText(this,"Ojala funcione :'v", Toast.LENGTH_LONG).show();
-        }*/
     }
 }
 
